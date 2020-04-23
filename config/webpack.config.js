5451dd36a22167c08e463895ddea8ddb3cb2afa5
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-04-23 22:49:17 +0200

    two webpack files in one folder

---
+++ config/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+// import path from 'path';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../', 'build')
+  }
+}
--- source/app.js
+++
@@ -1,3 +0,0 @@
-import {message, messageDOM} from './message'
-messageDOM('działam po boundlingu w przeglądarce')
-message('działam po boundlingu')
