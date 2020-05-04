3e9c586571fd6f5e7235c4629c13b143d31ba1b8
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-05-04 23:16:08 +0200

    graphics optimization

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -28,11 +28,24 @@ module.exports = {
       },
       {
         test: /\.(jpg|png|svg|gif|jpeg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name][contenthash:6].[ext]',
-          outputPath: 'images',
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name][contenthash:6].[ext]',
+            outputPath: 'images',
+          }
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              quality: 70,
+              progressive: true
+            }
+          }
         }
+        ]
+
       },
     ]
   },
