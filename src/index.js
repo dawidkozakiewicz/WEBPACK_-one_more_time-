d23aa9a1d9ca84764f49b86b4c150fe989d7d582
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-05-05 10:11:23 +0200

    using class

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import './components/footer'
 import info from './data/title.txt';
 import './sass/index.scss';
 import addImage from './tools/image';
+import Creator from './tools/creator'
+
 
 message(info);
 messageDOM(info);
 addImage('h1');
+
+
+const e1 = new Creator;
+e1.addBgc('red');
+
+const e2 = new Creator;
+e2.addBgc('green');
+
+const e3 = new Creator;
+e3.addBgc('orange');
+
